14cac91d643a0fc3c51f932911c575cc0ef417d8
engstk <eng.stk@sapo.pt>
2018-06-25 11:42:58 +0100

    cpufreq: Add blu_schedutil
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -518,9 +518,12 @@ extern struct cpufreq_governor cpufreq_gov_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+<<<<<<< HEAD
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDHORIZON)
 extern struct cpufreq_governor cpufreq_gov_schedhorizon;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedhorizon)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
+extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
 #endif
 
 
