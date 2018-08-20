15075f5587b07745f746bdf7bdd1cb4234f8653f
Parvinder Singh <psndna88@gmail.com>
2018-08-20 07:50:21 +0530

    [Squash] Disable tracing/logging at various places
    Squash of commits:
        disable tracing/logging at various places Part 1
        disable tracing/logging at various places Part 2
        disable tracing/logging at various places Part 3
        disable tracing/logging at various places Part 4
        disable tracing/logging at various places Part 5
        disable tracing/logging at various places Part 6
        disable tracing/logging at various places Part 7
        disable tracing/logging at various places rcu
    
    Signed-off-by: Parvinder Singh <psndna88@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -2689,7 +2689,7 @@ out:
 static int mmc_suspend(struct mmc_host *host)
 {
 	int err;
-	ktime_t start = ktime_get();
+//	ktime_t start = ktime_get();
 
 	MMC_TRACE(host, "%s: Enter\n", __func__);
 	err = _mmc_suspend(host, true);
@@ -2698,8 +2698,8 @@ static int mmc_suspend(struct mmc_host *host)
 		pm_runtime_set_suspended(&host->card->dev);
 	}
 
-	trace_mmc_suspend(mmc_hostname(host), err,
-			ktime_to_us(ktime_sub(ktime_get(), start)));
+//	trace_mmc_suspend(mmc_hostname(host), err,
+//			ktime_to_us(ktime_sub(ktime_get(), start)));
 	MMC_TRACE(host, "%s: Exit err: %d\n", __func__, err);
 	return err;
 }
@@ -2774,7 +2774,7 @@ out:
 static int mmc_resume(struct mmc_host *host)
 {
 	int err = 0;
-	ktime_t start = ktime_get();
+//	ktime_t start = ktime_get();
 
 	MMC_TRACE(host, "%s: Enter\n", __func__);
 	if (!(host->caps & MMC_CAP_RUNTIME_RESUME)) {
@@ -2784,8 +2784,8 @@ static int mmc_resume(struct mmc_host *host)
 	}
 	pm_runtime_enable(&host->card->dev);
 
-	trace_mmc_resume(mmc_hostname(host), err,
-			ktime_to_us(ktime_sub(ktime_get(), start)));
+//	trace_mmc_resume(mmc_hostname(host), err,
+//			ktime_to_us(ktime_sub(ktime_get(), start)));
 	MMC_TRACE(host, "%s: Exit err: %d\n", __func__, err);
 	return err;
 }
@@ -2854,7 +2854,7 @@ unhalt:
 static int mmc_runtime_suspend(struct mmc_host *host)
 {
 	int err;
-	ktime_t start = ktime_get();
+//	ktime_t start = ktime_get();
 
 	if (!(host->caps & MMC_CAP_AGGRESSIVE_PM))
 		return 0;
@@ -2871,8 +2871,8 @@ static int mmc_runtime_suspend(struct mmc_host *host)
 		pr_err("%s: error %d doing aggressive suspend\n",
 			mmc_hostname(host), err);
 
-	trace_mmc_runtime_suspend(mmc_hostname(host), err,
-			ktime_to_us(ktime_sub(ktime_get(), start)));
+//	trace_mmc_runtime_suspend(mmc_hostname(host), err,
+//			ktime_to_us(ktime_sub(ktime_get(), start)));
 	return err;
 }
 
@@ -2882,7 +2882,7 @@ static int mmc_runtime_suspend(struct mmc_host *host)
 static int mmc_runtime_resume(struct mmc_host *host)
 {
 	int err;
-	ktime_t start = ktime_get();
+//	ktime_t start = ktime_get();
 
 	if (!(host->caps & (MMC_CAP_AGGRESSIVE_PM | MMC_CAP_RUNTIME_RESUME)))
 		return 0;
@@ -2893,8 +2893,8 @@ static int mmc_runtime_resume(struct mmc_host *host)
 		pr_err("%s: error %d doing aggressive resume\n",
 			mmc_hostname(host), err);
 
-	trace_mmc_runtime_resume(mmc_hostname(host), err,
-			ktime_to_us(ktime_sub(ktime_get(), start)));
+//	trace_mmc_runtime_resume(mmc_hostname(host), err,
+//			ktime_to_us(ktime_sub(ktime_get(), start)));
 
 	return err;
 }
