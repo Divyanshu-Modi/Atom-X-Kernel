5d5f7843141b05ab76ccb4bc83e9206b79824a51
Oktapra Amtono <oktapra.amtono@gmail.com>
2020-10-19 20:00:28 +0700

    sched/fair: Tune the scheduler
    inspiration from Ktweak by @ tytydraco, thanks to him
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -77,8 +77,8 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_N
  * Minimal preemption granularity for CPU-bound tasks:
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity		= 2500000ULL;
-unsigned int normalized_sysctl_sched_min_granularity	= 2500000ULL;
+unsigned int sysctl_sched_min_granularity		= 1250000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= 1250000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
@@ -99,8 +99,8 @@ unsigned int sysctl_sched_child_runs_first __read_mostly = 1;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity		= 10000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity	= 10000000UL;
+unsigned int sysctl_sched_wakeup_granularity		= 5000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 5000000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
@@ -8509,7 +8509,7 @@ static struct task_struct *hisi_get_heaviest_task(
 	return max_preferred_tsk ? max_preferred_tsk : max_util_task;
 }
 
-static const unsigned int sched_nr_migrate_break = 32;
+static const unsigned int sched_nr_migrate_break = 8;
 static const unsigned int up_migration_util_filter = 25;
 
 /*
