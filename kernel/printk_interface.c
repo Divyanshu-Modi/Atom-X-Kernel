0667f9d5ee6ee273a4afd4affb8d753c5e52a619
Divyanshu-Modi <divyan.m05@gmail.com>
2021-04-23 20:51:40 +0530

    Revert "🎨 kernel: Enable printk logging by default"
    This reverts commit cf3b73ced4e59af2c689020ee90974ed06e407f9.
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/printk_interface.c
+++ kernel/printk_interface.c
@@ -1,25 +1,47 @@
-// SPDX-License-Identifier: GPL-2.0
-// Author: andip71, 17.11.2012
+/*
+ * Author: andip71, 17.11.2012
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 
 #include <linux/module.h>
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
 #include "printk_interface.h"
 
+
 int printk_mode;
 
+
 /* sysfs interface for printk mode */
+
 static ssize_t printk_mode_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-	if (printk_mode == 0) { // print current mode
+
+	// print current mode
+	if (printk_mode == 0)
+	{
 		return sprintf(buf, "printk mode: %d (disabled)", printk_mode);
-	} else {
+	}
+	else
+	{
 		return sprintf(buf, "printk mode: %d (enabled)", printk_mode);
 	}
+
 }
+
 
 static ssize_t printk_mode_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
+
 	unsigned int ret = -EINVAL;
 	int val;
 
@@ -27,14 +49,17 @@ static ssize_t printk_mode_store(struct kobject *kobj, struct kobj_attribute *at
 	ret = sscanf(buf, "%d", &val);
 
 	// check value and store if valid
-	if ((val == 0) ||  (val == 1)) {
+	if ((val == 0) ||  (val == 1))
+	{
 		printk_mode = val;
 	}
 
 	return count;
 }
 
+
 /* Initialize printk_mode sysfs folder */
+
 static struct kobj_attribute printk_mode_attribute =
 __ATTR(printk_mode, 0644, printk_mode_show, printk_mode_store);
 
@@ -53,27 +78,31 @@ static int __init printk_mode_init(void)
 {
 	int printk_mode_retval;
 
-	printk_mode_kobj = kobject_create_and_add("printk_mode", kernel_kobj);
+        printk_mode_kobj = kobject_create_and_add("printk_mode", kernel_kobj);
 
-	if (!printk_mode_kobj) {
-		return -ENOMEM;
+        if (!printk_mode_kobj) {
+                return -ENOMEM;
+        }
+
+        printk_mode_retval = sysfs_create_group(printk_mode_kobj, &printk_mode_attr_group);
+
+        if (printk_mode_retval)
+	{
+			kobject_put(printk_mode_kobj);
 	}
 
-	printk_mode_retval = sysfs_create_group(printk_mode_kobj, &printk_mode_attr_group);
-
-	if (printk_mode_retval) {
-		kobject_put(printk_mode_kobj);
-	}
-
-	printk_mode = 1; // initialize printk mode to 1 (enabled) as default
+	// initialize printk mode to 0 (disabled) as default
+	printk_mode = 0;
 
 	return (printk_mode_retval);
 }
+
 
 static void __exit printk_mode_exit(void)
 {
 	kobject_put(printk_mode_kobj);
 }
+
 
 /* define driver entry points */
 module_init(printk_mode_init);
