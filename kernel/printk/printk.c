a7c1889121adb2dbb29d7b30bd565ed64ee07c81
Divyanshu-Modi <divyan.m05@gmail.com>
2021-05-04 18:49:22 +0530

    Revert "printk: Add sys kernel interface to configure linux printk logging"
    This reverts commit 7182073fed630fd87ec4e620bf3b106055ee96e0.
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -49,8 +49,6 @@
 
 #include <asm/uaccess.h>
 
-#include "../printk_interface.h"
-
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
@@ -1692,10 +1690,6 @@ asmlinkage int vprintk_emit(int facility, int level,
 	/* cpu currently holding logbuf_lock in this function */
 	static unsigned int logbuf_cpu = UINT_MAX;
 
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-			return 0;
-
 	if (level == LOGLEVEL_SCHED) {
 		level = LOGLEVEL_DEFAULT;
 		in_sched = true;
@@ -1860,10 +1854,6 @@ EXPORT_SYMBOL(vprintk_emit);
 
 asmlinkage int vprintk(const char *fmt, va_list args)
 {
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-			return 0;
-
 	return vprintk_emit(0, LOGLEVEL_DEFAULT, NULL, 0, fmt, args);
 }
 EXPORT_SYMBOL(vprintk);
@@ -1874,10 +1864,6 @@ asmlinkage int printk_emit(int facility, int level,
 {
 	va_list args;
 	int r;
-
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-			return 0;
 
 	va_start(args, fmt);
 	r = vprintk_emit(facility, level, dict, dictlen, fmt, args);
@@ -1890,10 +1876,6 @@ EXPORT_SYMBOL(printk_emit);
 int vprintk_default(const char *fmt, va_list args)
 {
 	int r;
-
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-			return 0;
 
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
@@ -1941,10 +1923,6 @@ asmlinkage __visible int printk(const char *fmt, ...)
 	printk_func_t vprintk_func;
 	va_list args;
 	int r;
-
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-		return 0;
 
 	va_start(args, fmt);
 
