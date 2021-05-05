9f72af7bca181a67d7cf28cb28081f1b93daaa4a
Divyanshu-Modi <divyan.m05@gmail.com>
2021-05-05 11:57:30 +0530

    Input: touchscreen:Synaptics_tcm: rename to synaptics_tcm_f7a
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/input/touchscreen/synaptics_tcm_f7a/synaptics_tcm_core.c
+++ drivers/input/touchscreen/synaptics_tcm_f7a/synaptics_tcm_core.c
@@ -1918,7 +1918,7 @@ static int syna_tcm_enable_irq(struct syna_tcm_hcd *tcm_hcd, bool en, bool ns)
 
 		if (irq_freed) {
 			retval = request_threaded_irq(tcm_hcd->irq, NULL,
-					syna_tcm_isr, bdata->irq_flags | IRQF_PERF_CRITICAL,
+					syna_tcm_isr, bdata->irq_flags,
 					PLATFORM_DRIVER_NAME, tcm_hcd);
 			if (retval < 0) {
 				LOGE(tcm_hcd->pdev->dev.parent,
@@ -3433,17 +3433,17 @@ static int syna_tcm_probe(struct platform_device *pdev)
 
 	LOGV("INIT_WORK : syna_tcm_helper\n");
 	tcm_hcd->helper.workqueue =
-		alloc_workqueue("syna_tcm_helper", WQ_FREEZABLE | WQ_HIGHPRI | WQ_UNBOUND, 0);
+		create_singlethread_workqueue("syna_tcm_helper");
 	INIT_WORK(&tcm_hcd->helper.work, syna_tcm_helper_work);
 
 	LOGV("INIT_DELAYED_WORK : syna_tcm_watchdog\n");
 	tcm_hcd->watchdog.workqueue =
-		alloc_workqueue("syna_tcm_watchdog", WQ_FREEZABLE | WQ_HIGHPRI | WQ_UNBOUND, 0);
+		create_singlethread_workqueue("syna_tcm_watchdog");
 	INIT_DELAYED_WORK(&tcm_hcd->watchdog.work, syna_tcm_watchdog_work);
 
 	LOGV("INIT_DELAYED_WORK : syna_tcm_polling\n");
 	tcm_hcd->polling_workqueue =
-		alloc_workqueue("syna_tcm_polling", WQ_FREEZABLE | WQ_HIGHPRI | WQ_UNBOUND, 0);
+		create_singlethread_workqueue("syna_tcm_polling");
 	INIT_DELAYED_WORK(&tcm_hcd->polling_work, syna_tcm_polling_work);
 
 	/* add reset work by wanghan start */
@@ -3457,7 +3457,7 @@ static int syna_tcm_probe(struct platform_device *pdev)
 
 	LOGV("INIT_DELAYED_WORK : syna_tcm_glove\n");
 	tcm_hcd->glove.workqueue =
-		alloc_workqueue("syna_tcm_glove", WQ_FREEZABLE | WQ_HIGHPRI | WQ_UNBOUND, 0);
+		create_singlethread_workqueue("syna_tcm_glove");
 	INIT_DELAYED_WORK(&tcm_hcd->glove.work, syna_tcm_glove_work);
 
 	LOGV("enable interrupt\n");
@@ -3487,7 +3487,7 @@ static int syna_tcm_probe(struct platform_device *pdev)
 
 	LOGV("INIT_WORK : syna_tcm_module\n");
 	mod_pool.workqueue =
-		alloc_workqueue("syna_tcm_module", WQ_FREEZABLE | WQ_HIGHPRI | WQ_UNBOUND, 0);
+		create_singlethread_workqueue("syna_tcm_module");
 	INIT_WORK(&mod_pool.work, syna_tcm_module_work);
 	mod_pool.tcm_hcd = tcm_hcd;
 	mod_pool.queue_work = true;
