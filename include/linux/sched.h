c012de66d5c6989f90b576ac8a25fd4fb102aa24
Sultan Alsawaf <sultan@kerneltoast.com>
2021-05-06 15:14:05 +0530

    sched: Add API to migrate the current process to a given cpumask
    There are some chunks of code in the kernel running in process context
    where it may be helpful to run the code on a specific set of CPUs, such
    as when reading some CPU-intensive procfs files. This is especially
    useful when the code in question must run within the context of the
    current process (so kthreads cannot be used).
    
    Add an API to make this possible, which consists of the following:
    sched_migrate_to_cpumask_start():
     @old_mask: pointer to output the current task's old cpumask
     @DesT: pointer to a cpumask the current task should be moved to
    
    sched_migrate_to_cpumask_end():
     @old_mask: pointer to the old cpumask generated earlier
     @DesT: pointer to the dest cpumask provided earlier
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2542,6 +2542,11 @@ static inline bool cpupri_check_rt(void)
 }
 #endif
 
+void sched_migrate_to_cpumask_start(struct cpumask *old_mask,
+				    const struct cpumask *dest);
+void sched_migrate_to_cpumask_end(const struct cpumask *old_mask,
+				  const struct cpumask *dest);
+
 extern u32 sched_get_wake_up_idle(struct task_struct *p);
 extern int sched_set_wake_up_idle(struct task_struct *p, int wake_up_idle);
 
