7bdfd75732d44209a7cd076f4cfc7b349d448f9c
Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
2021-06-05 13:04:06 +0530

    arm64: fix endianness annotation for 'struct jit_ctx' and friends struct jit_ctx::image is used the store a pointer to the jitted intructions, which are always little-endian. These instructions are thus correctly converted from native order to little-endian before being stored but the pointer 'image' is declared as for native order values.
    Fix this by declaring the field as __le32* instead of u32*.
    Same for the pointer used in jit_fill_hole() to initialize
    the image.
    
    Signed-off-by: Luc Van Oostenryck <luc.vanoostenryck@gmail.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Change-Id: I8f00a950473046976040fed6f70c4d6104051fc3
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- arch/arm64/net/bpf_jit_comp.c
+++ arch/arm64/net/bpf_jit_comp.c
@@ -62,7 +62,7 @@ struct jit_ctx {
 	int tmp_used;
 	int epilogue_offset;
 	int *offset;
-	u32 *image;
+	__le32 *image;
 };
 
 static inline void emit(const u32 insn, struct jit_ctx *ctx)
@@ -122,7 +122,7 @@ static inline int bpf2a64_offset(int bpf_to, int bpf_from,
 
 static void jit_fill_hole(void *area, unsigned int size)
 {
-	u32 *ptr;
+	__le32 *ptr;
 	/* We are guaranteed to have aligned memory. */
 	for (ptr = area; size >= sizeof(u32); size -= sizeof(u32))
 		*ptr++ = cpu_to_le32(AARCH64_BREAK_FAULT);
@@ -789,7 +789,7 @@ void bpf_int_jit_compile(struct bpf_prog *prog)
 
 	/* 2. Now, the actual pass. */
 
-	ctx.image = (u32 *)image_ptr;
+	ctx.image = (__le32 *)image_ptr;
 	ctx.idx = 0;
 
 	build_prologue(&ctx);
