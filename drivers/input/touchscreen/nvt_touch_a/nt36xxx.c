536525cd86e38492164d4b5da589434fe2b1f4cd
Divyanshu-Modi <divyan.m05@gmail.com>
2021-07-08 16:32:23 +0530

    Input: touchscreen: nvt_touch_*: Adapt wakeup changes with new api backports
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- drivers/input/touchscreen/nvt_touch_a/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_a/nt36xxx.c
@@ -75,7 +75,7 @@ const uint16_t gesture_key_array[] = {
 bool enable_gesture_mode = false;
 bool delay_gesture = false;
 bool suspend_state = false;
-static struct wakeup_source gesture_wakelock;
+static struct wakeup_source *gesture_wakelock;
 
 int nvt_gesture_switch(struct input_dev *dev, unsigned int type, unsigned int code, int value)
 {
@@ -511,7 +511,7 @@ static irqreturn_t nvt_ts_irq_handler(int32_t irq, void *dev_id)
 {
 #if WAKEUP_GESTURE
 	if (bTouchIsAwake == 0) {
-		__pm_wakeup_event(&gesture_wakelock, msecs_to_jiffies(5000));
+		__pm_wakeup_event(gesture_wakelock, msecs_to_jiffies(5000));
 	}
 #endif
 
@@ -716,7 +716,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	ts->input_dev->event =nvt_gesture_switch;
 	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++)
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
-	wakeup_source_init(&gesture_wakelock, "gesture_wakelock");
+	gesture_wakelock = wakeup_source_register(NULL, "gesture_wakelock");
 #ifdef CONFIG_TOUCHSCREEN_COMMON
 	ret = tp_common_set_double_tap_ops(&double_tap_ops);
 #endif
--- drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_f7a/nt36xxx.c
@@ -89,7 +89,7 @@ bool enable_gesture_mode = false;
 EXPORT_SYMBOL(enable_gesture_mode);
 bool delay_gesture = false;
 bool suspend_state = false;
-static struct wakeup_source gesture_wakelock;
+static struct wakeup_source *gesture_wakelock;
 
 int nvt_gesture_switch(struct input_dev *dev, unsigned int type, unsigned int code, int value)
 {
@@ -562,7 +562,7 @@ static irqreturn_t nvt_ts_irq_handler(int32_t irq, void *dev_id)
 {
 #if WAKEUP_GESTURE
 	if (bTouchIsAwake == 0) {
-		__pm_wakeup_event(&gesture_wakelock, msecs_to_jiffies(5000));
+		__pm_wakeup_event(gesture_wakelock, msecs_to_jiffies(5000));
 	}
 #endif
 
@@ -762,7 +762,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++) {
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
 	}
-	wakeup_source_init(&gesture_wakelock, "gesture_wakelock");
+	gesture_wakelock = wakeup_source_register(NULL, "gesture_wakelock");
 #ifdef CONFIG_TOUCHSCREEN_COMMON
 	ret = tp_common_set_double_tap_ops(&double_tap_ops);
 #endif
