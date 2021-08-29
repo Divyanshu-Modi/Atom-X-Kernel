77510f36a4989ca55c537ac1a127ba5b83fa06d9
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-08-30 00:59:31 +0600

    drivers: power: [FIX] Remove duplicate case and attr
    CAF merge added POWER_SUPPLY_PROP_FG_RESET_CLOCK case in L4339. But Tulip already has this from the xiaomi longcheer imports. Remove the duplicate case.
    Also, fg_reset_clock attr in power_supply was added by the xiaomi lonhcheer imports. So remove the duplicate attr and move the modifications done by longcheer imports so that it matches with include/linux/power_supply.h
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -308,10 +308,6 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(pd_voltage_max),
 	POWER_SUPPLY_ATTR(pd_voltage_min),
 	POWER_SUPPLY_ATTR(sdp_current_max),
-#ifdef CONFIG_MACH_LONGCHEER
-	POWER_SUPPLY_ATTR(fg_reset_clock),
-	POWER_SUPPLY_ATTR(rerun_apsd),
-#endif
 	POWER_SUPPLY_ATTR(fcc_stepper_enable),
 	POWER_SUPPLY_ATTR(ignore_false_negative_isense),
 	POWER_SUPPLY_ATTR(battery_info),
@@ -323,6 +319,9 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(parallel_fcc_max),
 	POWER_SUPPLY_ATTR(min_icl),
 	POWER_SUPPLY_ATTR(fg_reset_clock),
+#ifdef CONFIG_MACH_LONGCHEER
+	POWER_SUPPLY_ATTR(rerun_apsd),
+#endif
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
